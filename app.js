b3748277e93eb69ba2589a9766baab9d28a97c97
sundar <gm49302@gmail.com>
2023-01-10 11:55:42 +0530

    done

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const submit = document
   .getElementById("submit")
   .addEventListener("click", function () {
     var number = document.getElementById("number").value;
-    document.querySelector(".result").innerHTML = number;
     const tempSelected = document.getElementById("temp-diff");
     const valueTemp = tempSelected.options[tempSelected.selectedIndex].value;
-    // console.log(valueTemp);
-    
+    if (valueTemp == "celcious") {
+      let farenheight = ((number - 32) * 5) / 9;
+      document.querySelector(".result").innerHTML = `${farenheight}°C`;
+    } else {
+      let celcious = (number * 9) / 5 + 32;
+      document.querySelector(".result").innerHTML = `${celcious}°F`;
+    }
   });
